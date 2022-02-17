6161ba3e4989a5c29ca73bef024e9e00eaf1395d
Juan Fernandez <juanfernandez_carc@hotmail.com>
2022-02-17 10:47:15 -0300

    Creation of the project, installation of django, guest class and connection with BD

---
+++ testApi/InvitedApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Invited(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    country = models.CharField(max_length=30)
+    phone = models.IntegerField(max_length=30)
+    job = models.CharField(max_length=30)
+
