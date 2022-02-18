f4fe0b16314c1d9fe523a040b9aa36feb73c88c3
Juan Fernandez <juanfernandez_carc@hotmail.com>
2022-02-18 00:23:12 -0300

    Implementation of api resources

--- testApi/api/admin.py
+++ testApi/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Invited
 
 # Register your models here.
+
+admin.site.register(Invited)
--- testApi/api/apps.py
+++ testApi/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class InvitedappConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'InvitedApp'
+    name = 'api'
